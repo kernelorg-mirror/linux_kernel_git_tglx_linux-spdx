0ee0dedd4f020bfd1f0aa44f92114e4fd005b6a5
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:26:46 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 249
    Based on 1 normalized pattern(s):
    
      as all part of the linux kernel this file is gpl
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-only
    
    has been chosen to replace the boilerplate/reference in 4 file(s).
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- net/wireless/wext-core.c
+++ net/wireless/wext-core.c
@@ -1,11 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * This file implement the Wireless Extensions core API.
  *
  * Authors :	Jean Tourrilhes - HPL - <jt@hpl.hp.com>
  * Copyright (c) 1997-2007 Jean Tourrilhes, All Rights Reserved.
  * Copyright	2009 Johannes Berg <johannes@sipsolutions.net>
- *
- * (As all part of the Linux kernel, this file is GPL)
  */
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
