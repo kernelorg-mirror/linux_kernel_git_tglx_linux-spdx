2e8ea0d7dd92e109fce6b188ae14784ee7cad098
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:26:59 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 319
    Based on 1 normalized pattern(s):
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license version 2 as
      published by the free software foundation this program is
      distributed as is without any warranty of any kind whether express
      or implied without even the implied warranty of merchantability or
      fitness for a particular purpose see the gnu general public license
      for more details
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-only
    
    has been chosen to replace the boilerplate/reference in 51 file(s).
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/power/supply/lego_ev3_battery.c
+++ drivers/power/supply/lego_ev3_battery.c
@@ -1,16 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Battery driver for LEGO MINDSTORMS EV3
  *
  * Copyright (C) 2017 David Lechner <david@lechnology.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
-
- * This program is distributed "as is" WITHOUT ANY WARRANTY of any
- * kind, whether express or implied; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <linux/delay.h>
