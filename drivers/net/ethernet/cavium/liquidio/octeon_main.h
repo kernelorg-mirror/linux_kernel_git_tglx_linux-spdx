8d749ebffd8b1f0f1f8eed2b65a2bff3feaca569
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:27:20 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 447
    Based on 1 normalized pattern(s):
    
      this file is free software you can redistribute it and or modify it
      under the terms of the gnu general public license version 2 as
      published by the free software foundation this file is distributed
      in the hope that it will be useful but as is and without any
      warranty without even the implied warranty of merchantability or
      fitness for a particular purpose title or noninfringement see the
      gnu general public license for more details
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-only
    
    has been chosen to replace the boilerplate/reference in 39 file(s).
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/net/ethernet/cavium/liquidio/octeon_main.h
+++ drivers/net/ethernet/cavium/liquidio/octeon_main.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /**********************************************************************
  * Author: Cavium, Inc.
  *
@@ -6,14 +7,6 @@
  *
  * Copyright (c) 2003-2016 Cavium, Inc.
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, Version 2, as
- * published by the Free Software Foundation.
- *
- * This file is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
  ***********************************************************************/
 /*! \file octeon_main.h
  *  \brief Host Driver: This file is included by all host driver source files
