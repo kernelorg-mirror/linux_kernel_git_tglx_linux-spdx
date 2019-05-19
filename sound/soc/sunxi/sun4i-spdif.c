e0189c20459988ca43bb283e6a1f52a553091130
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:26:01 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 119
    Based on 1 normalized pattern(s):
    
      released under the gpl this program is free software you can
      redistribute it and or modify it under the terms of the gnu general
      public license as published by the free software foundation either
      version 2 of the license or at your option any later version this
      program is distributed in the hope that it will be useful but
      without any warranty without even the implied warranty of
      merchantability or fitness for a particular purpose see the gnu
      general public license for more details
    
    
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-or-later
    
    has been chosen to replace the boilerplate/reference in 2 file(s).
    
    The result has been manually verified and compared against a license
    compliance dataset provided by ....
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- sound/soc/sunxi/sun4i-spdif.c
+++ sound/soc/sunxi/sun4i-spdif.c
@@ -1,20 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * ALSA SoC SPDIF Audio Layer
  *
  * Copyright 2015 Andrea Venturi <be17068@iperbole.bo.it>
  * Copyright 2015 Marcus Cooper <codekipper@gmail.com>
- *
- * Based on the Allwinner SDK driver, released under the GPL.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <linux/clk.h>
