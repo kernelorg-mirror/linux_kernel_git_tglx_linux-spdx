27217b395507bcd64db6f65f2de56b564bd9b727
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:27:19 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 441
    Based on 1 normalized pattern(s):
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license as published by
      the free software foundation version 2 of the license
    
    
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-only
    
    has been chosen to replace the boilerplate/reference in 315 file(s).
    
    The result has been manually verified and compared against a license
    compliance dataset provided by ....
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/platform/x86/surface3-wmi.c
+++ drivers/platform/x86/surface3-wmi.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  *  Driver for the LID cover switch of the Surface 3
  *
@@ -5,9 +6,6 @@
  */
 
 /*
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; version 2 of the License.
  */
 
 #include <linux/kernel.h>
