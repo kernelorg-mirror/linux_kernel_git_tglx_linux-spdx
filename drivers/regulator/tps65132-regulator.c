302a78ae3ede0e7611436779a6f538de0ea1ec4c
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:26:01 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 124
    Based on 1 normalized pattern(s):
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license as published by
      the free software foundation either version 2 of the license or at
      your option any later version this program is distributed as is
      without any warranty of any kind whether express or implied without
      even the implied warranty of merchantability or fitness for a
      particular purpose see the gnu general public license for more
      details
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-or-later
    
    has been chosen to replace the boilerplate/reference in 1 file(s).
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/regulator/tps65132-regulator.c
+++ drivers/regulator/tps65132-regulator.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * TI TPS65132 Regulator driver
  *
@@ -5,16 +6,6 @@
  *
  * Author: Venkat Reddy Talla <vreddytalla@nvidia.com>
  *		Laxman Dewangan <ldewangan@nvidia.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed "as is" WITHOUT ANY WARRANTY of any kind,
- * whether express or implied; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
  */
 
 #include <linux/delay.h>
