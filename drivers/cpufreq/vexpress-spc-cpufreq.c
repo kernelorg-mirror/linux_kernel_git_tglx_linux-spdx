0c3653c61f274a931b2cae1c551626d432f0cd51
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:26:59 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 319
    Based on 1 normalized pattern(s):
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license version 2 as
      published by the free software foundation this program is
      distributed as is without any warranty of any kind whether express
      or implied without even the implied warranty of merchantability or
      fitness for a particular purpose see the gnu general public license
      for more details
    
    
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-only
    
    has been chosen to replace the boilerplate/reference in 51 file(s).
    
    The result has been manually verified and compared against a license
    compliance dataset provided by ....
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/cpufreq/vexpress-spc-cpufreq.c
+++ drivers/cpufreq/vexpress-spc-cpufreq.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Versatile Express SPC CPUFreq Interface driver
  *
@@ -5,15 +6,6 @@
  *
  * Copyright (C) 2013 ARM Ltd.
  * Sudeep KarkadaNagesha <sudeep.karkadanagesha@arm.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed "as is" WITHOUT ANY WARRANTY of any
- * kind, whether express or implied; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
