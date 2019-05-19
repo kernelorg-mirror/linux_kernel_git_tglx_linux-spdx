1f4356dc65cc304f91350841b6be54ea50ab3eb2
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:25:43 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 2
    Based on 1 normalized pattern(s):
    
      permission is hereby granted to copy modify and redistribute this
      code in terms of the gnu library general public license version 2 or
      later at your option
    
    
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-or-later
    
    has been chosen to replace the boilerplate/reference in 1 file(s).
    
    The result has been manually verified and compared against a license
    compliance dataset provided by ....
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/mc146818rtc.h
+++ include/linux/mc146818rtc.h
@@ -1,11 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /* mc146818rtc.h - register definitions for the Real-Time-Clock / CMOS RAM
  * Copyright Torsten Duwe <duwe@informatik.uni-erlangen.de> 1993
  * derived from Data Sheet, Copyright Motorola 1984 (!).
  * It was written to be part of the Linux operating system.
- */
-/* permission is hereby granted to copy, modify and redistribute this code
- * in terms of the GNU Library General Public License, Version 2 or later,
- * at your option.
  */
 
 #ifndef _MC146818RTC_H
