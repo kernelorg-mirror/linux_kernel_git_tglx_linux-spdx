dc0adf0fd550b3f65e2dbedcfffdd216b6a89983
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:26:01 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 117
    Based on 1 normalized pattern(s):
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license as published by
      the free software foundation either version 2 of the license or at
      your option any later version due to this file being licensed under
      the gpl there is controversy over whether this permits you to write
      a module that includes this file without placing your module under
      the gpl please consult a lawyer for advice before doing this
    
    
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-or-later
    
    has been chosen to replace the boilerplate/reference in 2 file(s).
    
    The result has been manually verified and compared against a license
    compliance dataset provided by ....
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/security.h
+++ include/linux/security.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * Linux Security plug
  *
@@ -7,17 +8,6 @@
  * Copyright (C) 2001 James Morris <jmorris@intercode.com.au>
  * Copyright (C) 2001 Silicon Graphics, Inc. (Trust Technology Group)
  * Copyright (C) 2016 Mellanox Techonologies
- *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
- *
- *	Due to this file being licensed under the GPL there is controversy over
- *	whether this permits you to write a module that #includes this file
- *	without placing your module under the GPL.  Please consult a lawyer for
- *	advice before doing this.
- *
  */
 
 #ifndef __LINUX_SECURITY_H
