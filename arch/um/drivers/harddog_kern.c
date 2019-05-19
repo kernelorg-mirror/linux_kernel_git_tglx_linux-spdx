d1246fa18df850d42e4162ee99eedbdcdd9b0428
Thomas Gleixner <tglx@linutronix.de>
2019-05-19 13:25:58 +0100

    treewide: Replace GPLv2 boilerplate/reference with SPDX - rule 100
    Based on 2 normalized pattern(s):
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license as published by
      the free software foundation either version 2 of the license or at
      your option any later version neither [alan] [cox] [nor] [cymrunet]
      [ltd] admit liability nor provide warranty for any of this software
      this material is provided as is and at no charge
    
    
      this program is free software you can redistribute it and or modify
      it under the terms of the gnu general public license as published by
      the free software foundation either version 2 of the license or at
      your option any later version neither [sven] [anders] [marcus]
      [junker] [nor] [anduras] [ag] admit liability nor provide warranty
      for any of this software this material is provided as is and at no
      charge
    
    
    
    extracted by the scancode license scanner the SPDX license identifier
    
      GPL-2.0-or-later
    
    has been chosen to replace the boilerplate/reference in 12 file(s).
    
    The result has been manually verified and compared against a license
    compliance dataset provided by ....
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/um/drivers/harddog_kern.c
+++ arch/um/drivers/harddog_kern.c
@@ -1,18 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /* UML hardware watchdog, shamelessly stolen from:
  *
  *	SoftDog	0.05:	A Software Watchdog Device
  *
  *	(c) Copyright 1996 Alan Cox <alan@redhat.com>, All Rights Reserved.
  *				http://www.redhat.com
- *
- *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License
- *	as published by the Free Software Foundation; either version
- *	2 of the License, or (at your option) any later version.
- *
- *	Neither Alan Cox nor CymruNet Ltd. admit liability nor provide
- *	warranty for any of this software. This material is provided
- *	"AS-IS" and at no charge.
  *
  *	(c) Copyright 1995    Alan Cox <alan@lxorguk.ukuu.org.uk>
  *
